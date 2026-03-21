258e72925863414d6d6c28f0f8f1b9570b92eb34
yat0o1 <sg9462054@gmail.com>
2026-03-21 17:59:10 +0500

    edited main: added localhost origins

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="Tap.im API", version="1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5500", "http://127.0.0.1:5500"],
+    allow_origins=["http://localhost:5500", "http://127.0.0.1:5500", "http://localhost:5173", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
